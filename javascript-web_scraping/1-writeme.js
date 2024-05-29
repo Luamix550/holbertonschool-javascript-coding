6c954a89aa0fc9cc0d974648aae6935f535451e3
Luamix <luizalfonso611@gmail.com>
2024-05-28 22:37:10 -0500

    Task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
 const filePath = process.argv[2];
-const textData = process.argv[3];
+const content = process.argv[3];
 
-fs.writeFile(filePath, textData, 'utf-8', function (err) {
-  if (err) { console.log(err); }
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
 });
