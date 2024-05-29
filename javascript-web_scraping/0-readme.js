1a0206f4435f39b181d6eb29761b45ab89820577
Luamix <luizalfonso611@gmail.com>
2024-05-28 22:27:19 -0500

    Task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
 
+const fs = require('fs');
 const filePath = process.argv[2];
 
-function readFileAndPrint(file) {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(data);
-    }
-  });
-}
-readFileAndPrint(filePath);
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
