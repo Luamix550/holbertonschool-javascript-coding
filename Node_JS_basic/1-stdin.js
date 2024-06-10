1d629ffce5af87c1dee98ce48ffef3678ebb266e
Luamix <luizalfonso611@gmail.com>
2024-06-10 11:04:54 -0500

    Task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', function() {
+  const input = process.stdin.read();
+    if (input !== null) {
+      const name = input.toString()
+      process.stdout.write('Your name is: ' + name + '\n');
+    }
+});
+
+process.stdin.on('end', function() {
+  process.stdout.write('This important software is now closing\n');
+});
