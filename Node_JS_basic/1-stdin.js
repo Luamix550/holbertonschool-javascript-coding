a1e7f8eb5da0748bd5823c8e5deb80aca405dd7b
Luamix <luizalfonso611@gmail.com>
2024-06-10 11:18:55 -0500

    Task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
-process.stdin.on('readable', function() {
+
+process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
-    process.stdout.write('Your name is: ' + input);
-    }
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
