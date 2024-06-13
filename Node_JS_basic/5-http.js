7f904bbb3e291c65cbe76cfd63d4446a8b491747
Luamix <luizalfonso611@gmail.com>
2024-06-12 22:24:07 -0500

    Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,26 +1,25 @@
-/* eslint-disable */
 const http = require('http');
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
+const args = process.argv.slice(2);
 const DATABASE = args[0];
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  const { url } = req;
-
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
 
   if (url === '/') {
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
+      const studentsList = await countStudents(DATABASE);
+      res.end(studentsList);
     } catch (error) {
       res.end(error.message);
     }
