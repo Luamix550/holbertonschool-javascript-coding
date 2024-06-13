8f8d5d68c4dcd08a743d2e80b1b8d3a16607b9f6
Luamix <luizalfonso611@gmail.com>
2024-06-12 22:28:24 -0500

    Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,7 +1,8 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
 
 const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
 const DATABASE = args[0];
 
 const hostname = '127.0.0.1';
@@ -14,23 +15,22 @@ const app = http.createServer(async (req, res) => {
   const { url } = req;
 
   if (url === '/') {
-    res.end('Hello Holberton School!');
+    res.write('Hello Holberton School!');
   } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const studentsList = await countStudents(DATABASE);
-      res.end(studentsList);
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
-  } else {
-    res.statusCode = 404;
-    res.end();
   }
+  res.statusCode = 404;
+  res.end();
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
